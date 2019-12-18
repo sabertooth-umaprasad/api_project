42efcc65cd3d35bd8b624a468aa753c88efcc970
Adil Warsi <shubham.paliwal@sabertoothtech.in>
2019-12-18 17:54:31 +0530

    book title field remove

--- api_project_app/models.py
+++ api_project_app/models.py
@@ -2,4 +2,3 @@ from django.db import models
 
 class Books(models.Model):
     book_name = models.CharField(max_length=50,null=True,blank=True)
-    book_title = models.CharField(max_length=50,null=True,blank=True)
